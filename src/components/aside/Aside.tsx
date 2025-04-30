3274e818ce084e579e3ba60d87ba3ae87a2f7025
Andrew(Shendru) <dev.andrewearl@gmail.com>
2025-05-01 01:19:38 +0800

    first commit

---
+++ src/components/aside/Aside.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Aside() {
+  return <aside className="bg-transparent w-[6rem] h-full">Aside</aside>;
+}
+
+export default Aside;
---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NavBar() {
+  return <header className="bg-transparent h-[5rem]">NavBar</header>;
+}
+
+export default NavBar;
