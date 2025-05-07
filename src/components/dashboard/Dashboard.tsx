4319170a90acc76f2f204c18e71208715453efd9
shendru <dev.andrewearl@gmail.com>
2025-05-07 21:23:29 +0800

    Layout_02

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -4,8 +4,10 @@ import GoatTestImage from "../../assets/goat-test-img.jpg";
 function Dashboard() {
   return (
     <section className="bg-green-400 flex-1 flex">
-     <div className="bg-red-500 w-[70%]">
-      
+     <div className="bg-red-500 w-[70%] flex flex-row">
+     <div></div>
+     <div></div>
+
      </div>
      <div className="flex-1 bg-stone-700"></div>
     </section>
